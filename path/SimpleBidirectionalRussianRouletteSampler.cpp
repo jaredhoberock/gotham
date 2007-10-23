3795d34a2fcb34baa20fa200ff9adde346ce87e1
jaredhoberock <jaredhoberock@gmail.com>
2007-10-23 00:23:31 +0000

    Correctly account for eye and light subpaths of length 0.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1070 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- path/SimpleBidirectionalRussianRouletteSampler.cpp
+++ path/SimpleBidirectionalRussianRouletteSampler.cpp
@@ -23,21 +23,55 @@ bool SimpleBidirectionalRussianRouletteSampler
                   const HyperPoint &x,
                   Path &p)
 {
+  // clear the Path to begin
+  p.clear();
+
   // shorthand
   const RussianRoulette *roulette = mRoulette.get();
 
+  gpcpu::float2 &termination = p.getTerminationProbabilities();
+  size_t justAdded[2];
+
   // insert an eye vertex
-  if(p.insert(0, scene.getSensors(), false,
-              x[2][0], x[2][1], x[2][2], x[2][3]) == Path::INSERT_FAILED) return false;
+  float rr = (*roulette)();
+  if(x[2][4] < rr)
+  {
+    // if we don't find a vertex when we said we would look,
+    // we MUST return a failure; otherwise, we will bias results
+    // towards shorter paths
+    // XXX TODO make a similar insert() method which accepts a RussianRoulette object
+    justAdded[0] = p.insert(0, scene.getSensors(), false, x[2][0], x[2][1], x[2][2], x[2][3]);
+    if(justAdded[0] == Path::INSERT_FAILED) return false;
+    p[justAdded[0]].mPdf *= rr;
+    p[justAdded[0]].mAccumulatedPdf *= rr;
+  } // end if
+  else
+  {
+    justAdded[0] = Path::ROULETTE_TERMINATED;
+    termination[0] = 1.0f - rr;
+  } // end else
 
   // insert a light vertex
-  if(p.insert(p.size()-1, scene.getEmitters(), true,
-              x[1][0], x[1][1], x[1][2], x[1][3]) == Path::INSERT_FAILED) return false;
+  rr = (*roulette)();
+  if(x[1][4] < rr)
+  {
+    // if we don't find a vertex when we said we would look,
+    // we MUST return a failure; otherwise, we will bias results
+    // towards shorter paths
+    // XXX TODO make a similar insert() method which accepts a RussianRoulette object
+    justAdded[1] = p.insert(p.size()-1, scene.getEmitters(), true, x[1][0], x[1][1], x[1][2], x[1][3]);
+    if(justAdded[1] == Path::INSERT_FAILED) return false;
+    p[justAdded[1]].mPdf *= rr;
+    p[justAdded[1]].mAccumulatedPdf *= rr;
+  } // end if
+  else
+  {
+    justAdded[1] = Path::ROULETTE_TERMINATED;
+    termination[1] = 1.0f - rr;
+  } // end else
 
   size_t i = 2;
-  size_t justAdded[2] = {0, p.size()-1};
   bool subpath = false;
-  gpcpu::float2 &termination = p.getTerminationProbabilities();
   while(p.getSubpathLengths().sum() < mMaxPathLength
         && i < x.size())
   {
@@ -88,115 +122,12 @@ bool SimpleBidirectionalRussianRouletteSampler
     p[i] = p[j];
   } // end for i
 
-  // connect the two subpaths
-  p.connect(p[p.getSubpathLengths()[0]-1], p[p.getSubpathLengths()[0]]);
+  // connect the two subpaths if both of them exist
+  if(p.getSubpathLengths()[0] != 0 && p.getSubpathLengths()[1] != 0)
+    p.connect(p[p.getSubpathLengths()[0]-1], p[p.getSubpathLengths()[0]]);
 
-  return true;
+  return p.getSubpathLengths().sum() != 0;
 } // end SimpleBidirectionalRussianRouletteSampler::constructPath()
-
-//bool SimpleBidirectionalRussianRouletteSampler
-//  ::constructPath(const Scene &scene,
-//                  const HyperPoint &x,
-//                  Path &p)
-//{
-//  size_t i = 2;
-//  size_t justAdded[2] = {0, p.size()-1};
-//  bool subpath = false;
-//  if(x[0][4] > 0.5f) subpath = true;
-//
-//  if(!subpath)
-//  {
-//    // insert an eye vertex
-//    if(p.insert(0, scene.getSensors(), false,
-//                x[2][0], x[2][1], x[2][2], x[2][3]) == Path::NULL_VERTEX) return false;
-//
-//    // insert a light vertex
-//    if(p.insert(p.size()-1, scene.getEmitters(), true,
-//                x[1][0], x[1][1], x[1][2], x[1][3]) == Path::NULL_VERTEX) return false;
-//
-//    while(p.getSubpathLengths().sum() < mMaxPathLength
-//          && i < x.size())
-//    {
-//      if(justAdded[subpath] != Path::NULL_VERTEX)
-//      {
-//        if(i == 2)
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, true, false,
-//                                                                      x[0][0], x[0][1], x[0][2], x[0][3], &mRoulette);
-//        } // end if
-//        else if(i == 3)
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, false, false,
-//                                                                      x[i][0], x[i][1], x[i][2], x[i][3], &mRoulette);
-//        } // end else if
-//        else
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, !subpath, true,
-//                                                                      x[i][0], x[i][1], x[i][2], x[i][3], &mRoulette);
-//        } // end else
-//      } // end if
-//
-//      ++i;
-//      subpath = !subpath;
-//    } // end while
-//  } // end if
-//  else
-//  {
-//    // insert a light vertex
-//    if(p.insert(p.size()-1, scene.getEmitters(), true,
-//                x[2][0], x[2][1], x[2][2], x[2][3]) == Path::NULL_VERTEX) return false;
-//
-//    // insert an eye vertex
-//    if(p.insert(0, scene.getSensors(), false,
-//                x[1][0], x[1][1], x[1][2], x[1][3]) == Path::NULL_VERTEX) return false;
-//
-//    i = 3;
-//
-//    // XXX can't start from 2
-//    // but if we start from 3, we will skip using one of the points
-//    //i = 2;
-//    while(p.getSubpathLengths().sum() < mMaxPathLength
-//          && i < x.size())
-//    {
-//      if(justAdded[subpath] != Path::NULL_VERTEX)
-//      {
-//        if(!subpath && p.getSubpathLengths()[0] == 1)
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, true, false,
-//                                                                      x[0][0], x[0][1], x[0][2], x[0][3], &mRoulette);
-//        } // end if
-//        else if(subpath && p.getSubpathLengths()[1] == 1)
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, false, false,
-//                                                                      x[i][0], x[i][1], x[i][2], x[i][3], &mRoulette);
-//        } // end else if
-//        else
-//        {
-//          justAdded[subpath] = p.insertRussianRouletteWithTermination(justAdded[subpath], &scene, !subpath, true,
-//                                                                      x[i][0], x[i][1], x[i][2], x[i][3], &mRoulette);
-//        } // end else
-//      } // end if
-//
-//      ++i;
-//      subpath = !subpath;
-//    } // end while
-//  } // end else
-//
-//  // shuffle the light path so its last light vertex
-//  // immediately follows the last eye vertex
-//  size_t j = p.size() - p.getSubpathLengths()[1];
-//  for(size_t i = p.getSubpathLengths()[0];
-//      j != p.size();
-//      ++i, ++j)
-//  {
-//    p[i] = p[j];
-//  } // end for i
-//
-//  // connect the two subpaths
-//  p.connect(p[p.getSubpathLengths()[0]-1], p[p.getSubpathLengths()[0]]);
-//
-//  return true;
-//} // end SimpleBidirectionalRussianRouletteSampler::constructPath()
 
 void SimpleBidirectionalRussianRouletteSampler
   ::evaluate(const Scene &scene,
@@ -206,57 +137,84 @@ void SimpleBidirectionalRussianRouletteSampler
   // shorthand
   const RussianRoulette *roulette = mRoulette.get();
 
-  size_t totalLength = p.getSubpathLengths().sum();
   size_t eyeLength = p.getSubpathLengths()[0];
   size_t lightLength = p.getSubpathLengths()[1];
 
   float w;
-  const PathVertex &e = p[eyeLength-1];
-  const PathVertex &l = p[eyeLength];
+  float pdf;
+  Spectrum L;
+  bool addResult = false;
 
-  // don't connect specular surfaces to anything
-  if(e.mScattering->isSpecular() || l.mScattering->isSpecular()) return;
-
-  Spectrum L = e.mThroughput * l.mThroughput;
-
-  // XXX PERF: make compute throughput take the connection vector and geometric term
-  L *= p.computeThroughputAssumeVisibility(eyeLength, e,
-                                           lightLength, l);
-
-  // both subpaths should have at least one vertex
-  assert(eyeLength != 0 && lightLength != 0);
-  w = Path::computePowerHeuristicWeight(scene, 1, 1, &l, lightLength, p.getSubpathLengths()[1],
-                                        &e, eyeLength, p.getSubpathLengths()[0],
-                                        e.mToNext, e.mNextGeometricTerm, *roulette);  //// compute the weight before casting the ray
-  //if(eyeLength != 0 && lightLength != 0)
-  //{
-  //  w = Path::computePowerHeuristicWeightWithTermination(scene, &l, lightLength, p.getSubpathLengths()[1],
-  //                                        &e, eyeLength, p.getSubpathLengths()[0],
-  //                                        e.mToNext, e.mNextGeometricTerm, *roulette);
-  //} // end if
-  //else if(eyeLength == 0)
-  //{
-  //  w = Path::computePowerHeuristicWeightWithTerminationEyeSubpaths(scene, &l, lightLength, p.getSubpathLengths()[1],
-  //                                                   0, 0, p.getSubpathLengths()[0],
-  //                                                   *roulette);
-  //  w = 1.0f / (w + 1.0f);
-  //} // end else if
-  //else
-  //{
-  //  w = Path::computePowerHeuristicWeightWithTerminationLightSubpaths(scene,
-  //                                                     0, 0, p.getSubpathLengths()[1],
-  //                                                     &e, eyeLength, p.getSubpathLengths()[0],
-  //                                                     *roulette);
-  //  w = 1.0f / (w + 1.0f);
-  //} // end else
-
-  if(!L.isBlack()
-     && w != 0
-     && !scene.intersect(Ray(e.mDg.getPoint(), l.mDg.getPoint())))
+  if(eyeLength != 0 && lightLength != 0)
   {
-    //// implement MIS weights
-    //w = 1.0f / (p.getSubpathLengths().sum()-1);
+    const PathVertex &e = p[eyeLength-1];
+    const PathVertex &l = p[eyeLength];
 
+    // compute pdf of the entire Path
+    pdf = e.mAccumulatedPdf * l.mAccumulatedPdf;
+
+    // don't connect specular surfaces to anything
+    // if we used the correct RussianRoulette object, this should never happen
+    if(e.mScattering->isSpecular() || l.mScattering->isSpecular()) return;
+
+    // start with the throughput of the connection
+    // XXX PERF: make compute throughput take the connection vector and geometric term
+    L = p.computeThroughputAssumeVisibility(eyeLength, e,
+                                             lightLength, l);
+
+    // modulate by subpaths' throughput
+    L *= e.mThroughput * l.mThroughput;
+
+    // compute MIS weight
+    w = Path::computePowerHeuristicWeight(scene, 0, 0, &l, lightLength, p.getSubpathLengths()[1],
+                                          &e, eyeLength, p.getSubpathLengths()[0],
+                                          e.mToNext, e.mNextGeometricTerm, *roulette);
+
+    // add a result if the path carries throughput
+    addResult = w != 0 && !L.isBlack() && !scene.intersect(Ray(e.mDg.getPoint(), l.mDg.getPoint()));
+  } // end if
+  else if(lightLength != 0)
+  {
+    const PathVertex &l = p[0];
+
+    // compute pdf
+    pdf = l.mAccumulatedPdf;
+
+    // evaluate sensor at the end of the light path
+    L = l.mThroughput * l.mSensor->evaluate(l.mToNext, l.mDg);
+
+    // compute MIS weight
+    w = Path::computePowerHeuristicWeightEyeSubpaths(scene, 0, &l, lightLength, p.getSubpathLengths()[1],
+                                                     0, 0, p.getSubpathLengths()[0],
+                                                     *roulette);
+    w = 1.0f / (w + 1.0f);
+
+    // add a result if L isn't black & w != 0
+    addResult = !L.isBlack() && w != 0;
+  } // end else if
+  else if(eyeLength != 0)
+  {
+    const PathVertex &e = p[eyeLength-1];
+
+    // compute pdf
+    pdf = e.mAccumulatedPdf;
+
+    // evaluate emission at the end of the eye path
+    L = e.mThroughput * e.mEmission->evaluate(e.mToPrev, e.mDg);
+
+    // compute MIS weight
+    w = Path::computePowerHeuristicWeightLightSubpaths(scene, 0,
+                                                       0, 0, p.getSubpathLengths()[1],
+                                                       &e, eyeLength, p.getSubpathLengths()[0],
+                                                       *roulette);
+    w = 1.0f / (w + 1.0f);
+
+    // add a result if L isn't black & w != 0
+    addResult = !L.isBlack() && w != 0;
+  } // end else
+
+  if(addResult)
+  {
     // add a new result
     results.resize(results.size() + 1);
     Result &r = results.back();
@@ -265,7 +223,7 @@ void SimpleBidirectionalRussianRouletteSampler
     r.mThroughput = L;
 
     // set pdf, weight, and (s,t)
-    r.mPdf = e.mAccumulatedPdf * l.mAccumulatedPdf * p.getTerminationProbabilities().product();
+    r.mPdf = pdf * p.getTerminationProbabilities().product();
 
     r.mWeight = w;
     r.mEyeLength = eyeLength;
