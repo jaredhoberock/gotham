dd23509a12e2a0dce5a32c48a025fd3a99f79b20
jaredhoberock <jaredhoberock@gmail.com>
2007-10-29 20:12:16 +0000

    Correctly handle all cases of Paths.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1079 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- path/PathToImage.cpp
+++ path/PathToImage.cpp
@@ -12,7 +12,21 @@ void PathToImage
              float &u,
              float &v) const
 {
-  if(r.mEyeLength <= 1)
+  if(r.mEyeLength > 1)
+  {
+    // the first coordinate naturally corresponds to (u,v)
+    u = x[0][0];
+    v = x[0][1];
+  } // end if
+  else if(r.mEyeLength == 0)
+  {
+    unsigned int endOfLightPath = xPath.getSubpathLengths().sum() - r.mLightLength;
+    ::Vector w = xPath[endOfLightPath+1].mDg.getPoint();
+    w -= xPath[endOfLightPath].mDg.getPoint();
+    xPath[endOfLightPath].mSensor->invert(w, xPath[endOfLightPath].mDg,
+                                          u, v);
+  } // end if
+  else if(r.mEyeLength == 1)
   {
     // we need to invert the sensor function
     unsigned int endOfLightPath = xPath.getSubpathLengths().sum() - r.mLightLength;
@@ -21,12 +35,6 @@ void PathToImage
     xPath[0].mSensor->invert(w, xPath[0].mDg,
                              u, v);
   } // end if
-  else
-  {
-    // the first coordinate naturally corresponds to (u,v)
-    u = x[0][0];
-    v = x[0][1];
-  } // end else
 } // end PathToImage::evaluate()
 
 void PathToImage
@@ -38,4 +46,3 @@ void PathToImage
 {
   return evaluate(r,x,xPath,u,v);
 } // end PathToImage::operator()()
-
