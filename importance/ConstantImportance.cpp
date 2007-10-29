beb8fb5b1097462910bd9a9dd21a49736591f49d
jaredhoberock <jaredhoberock@gmail.com>
2007-10-29 20:11:24 +0000

    Return a constant.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1078 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- importance/ConstantImportance.cpp
+++ importance/ConstantImportance.cpp
@@ -10,16 +10,17 @@ float ConstantImportance
              const Path &xPath,
              const std::vector<PathSampler::Result> &results)
 {
-  //if(results.empty()) return 0;
-  //return 1.0;
+  if(results.empty()) return 0;
+  return 1.0;
 
-  float result = 0;
-  for(std::vector<PathSampler::Result>::const_iterator r = results.begin();
-      r != results.end();
-      ++r)
-  {
-    result += 1.0f / r->mPdf;
-  } // end for r
+  //float result = 0;
+  //for(std::vector<PathSampler::Result>::const_iterator r = results.begin();
+  //    r != results.end();
+  //    ++r)
+  //{
+  //  result += 1.0f / r->mPdf;
+  //} // end for r
 
-  return result;
+  //return result;
 } // end ConstantImportance::operator()()
+
