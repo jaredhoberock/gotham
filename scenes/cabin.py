fd6daaa96af4d4304ff5916a5234f4d4443ecdc5
jaredhoberock <jaredhoberock@gmail.com>
2007-12-02 13:09:12 +0000

    Create a separate directory to hold scenes. Move pbslc.py into api and rename is pbslc. Install pbscl to $GOTHAMHOME/bin.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1148 3bb10773-34f4-0310-b88c-ec9d511ba0d9

---
+++ scenes/cabin.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import api
+import objtogoth
+
+g = api.Gotham2()
+
+g.material('matte', "Kd", (1.0,1.0,1.0))
+(vertices, uvs, indices) = objtogoth.objtogoth("../../../data/geometry/obj/cabin.obj")
+g.mesh(vertices, uvs, indices)
+
+# light
+g.material('light', 'power', (3500 * 500,3500 * 500,3500 * 500))
+g.sphere(242, 33, -88, 5)
+
+#g.attribute("path::sampler", "kelemen")
+g.attribute("path::sampler", "simpleforwardrussianroulette")
+g.attribute("mutator::strategy", "stratified")
+g.attribute("path::maxlength", "10")
+g.attribute("renderer::algorithm", "metropolis")
+g.attribute("mutator::largestep", "0.001")
+g.attribute("importance::function", "equalvisit")
+#g.attribute("renderer::algorithm", "debug")
+g.attribute("path::russianroulette::function", "constant")
+g.attribute("path::russianroulette::continueprobability", "0.5")
+
+(w,h) = (800,400)
+g.pushMatrix()
+
+g.lookAt( (  29.7108,  18.1446,  -107.85),
+          (  30.6369,  18.2885,  -108.199),
+          (-0.138807, 0.989526, 0.039636) )
+
+g.camera(float(w) / h, 45.0, 0.01)
+g.popMatrix()
+
+g.render((w,h), 32)
+#g.render((w,h), 1)
+
---
+++ scenes/screenfillingquad.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import api
+
+g = api.Gotham2()
+
+#g.attribute("renderer::algorithm", "debug")
+g.attribute("path::sampler", "kajiya")
+g.attribute("path::maxlength", "10")
+#g.attribute("renderer::algorithm", "metropolis")
+
+g.material('matte', 'Kd', (1,1,1))
+points = [-1.0, -1.0, 0,
+           1.0, -1.0, 0,
+           1.0,  1.0, 0,
+          -1.0,  1.0, 0]
+triangles = [0, 1, 2,
+             0, 2, 3]
+g.mesh(points, triangles)
+
+# put a dim light behind the camera
+g.material('light', 'power', (20,20,20))
+points = [ 1.0, -1.0, 3,
+          -1.0, -1.0, 3,
+          -1.0,  1.0, 3,
+           1.0,  1.0, 3]
+g.mesh(points, triangles)
+
+# put a very bright light behind the quad
+g.material('light', 'power', (2000000,2000000,2000000))
+points = [-1.0, -1.0, -3,
+           1.0, -1.0, -3,
+           1.0,  1.0, -3,
+          -1.0,  1.0, -3]
+g.mesh(points, triangles)
+
+(w,h) = (512,512)
+
+g.pushMatrix()
+# why is 1.75 the magic number?
+g.translate(0,0,1.75)
+aspect = float(w) / h
+g.camera(aspect, 60.0, 0.0)
+g.popMatrix()
+
+g.render((w,h))
+
