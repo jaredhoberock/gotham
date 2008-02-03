b5c27738a3dcbf8cd2fe3a86ea2222b7c4cb42a4
jaredhoberock <jaredhoberock@gmail.com>
2008-02-03 01:13:58 +0000

    Add a shader which mimics Gelato's veinedmarble.gsl.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1430 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- shading/noises.h
+++ shading/noises.h
@@ -7,6 +7,7 @@
 #define NOISES_H
 
 #include "noise.h"
+#include "stdshader.h"
 #include "../geometry/Point.h"
 
 inline float snoise(const Point &x)
@@ -23,6 +24,40 @@ inline float snoise(const float x)
 {
   return gotham::noise(x,0,0);
 } // end snoise()
+
+inline Vector vsnoise(const Point &x)
+{
+  // XXX This is almost definitely not robust
+  return Vector(snoise(x),
+                snoise(x + Point(1,1,1)),
+                snoise(x + Point(2,2,2)));
+} // end vsnoise()
+
+#ifndef FILTLOW
+#  define FILTLOW 0.2
+#endif
+#ifndef FILTHIGH
+#  define FILTHIGH 0.75
+#endif
+
+inline Vector vectorFbm(const Point &p,
+                        int octaves, float lacunarity, float gain)
+{
+  float amp = 1.0f;
+  Point pp = p;
+  Vector sum(0,0,0);
+
+  for(int i = 0; i < octaves; ++i)
+  {
+    sum += amp * vsnoise(pp);
+    amp *= gain;
+
+    // multiply and offset
+    pp = pp * lacunarity + Point(1,1,1);
+  } // end for i
+
+  return sum;
+} // end vectorFbm()
 
 inline float periodicFbm(float p, float period, int octaves, float gain)
 {
