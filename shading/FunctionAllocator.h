12cf782c2c7093bdb790cda6ee1dfd3f32aca78b
jaredhoberock <jaredhoberock@gmail.com>
2008-03-03 20:03:33 +0000

    The previous implementation wasn't portable to 32b.
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1452 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- shading/FunctionAllocator.h
+++ shading/FunctionAllocator.h
@@ -53,7 +53,7 @@ class FunctionAllocator
       unsigned char mFill[size];
     }; // end Accomodator
 
-    typedef Accomodator<9 * sizeof(size_t)> Block;
+    typedef Accomodator<18 * sizeof(int)> Block;
 
   protected:
 
