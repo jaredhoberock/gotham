9db3b902103f6a1411c75e31fb9b642d5b49d3c6
jaredhoberock <jaredhoberock@gmail.com>
2008-09-16 15:17:39 +0000

    Draw our own sphere, don't rely on glu
    git-svn-id: svn+ssh://jhoberock-dt/home/jhoberock/svnbase/jared/src/gotham@1673 3bb10773-34f4-0310-b88c-ec9d511ba0d9

--- rasterizables/RasterizableSphere.cpp
+++ rasterizables/RasterizableSphere.cpp
@@ -4,6 +4,7 @@
  */
 
 #include "RasterizableSphere.h"
+#include "../geometry/Mappings.h"
 #include <GL/glew.h>
 
 RasterizableSphere
@@ -25,8 +26,58 @@ void RasterizableSphere
   glPushMatrix();
 
   glTranslatef(mCenter[0], mCenter[1], mCenter[2]);
+  
+  size_t uDivisions = 20;
+  size_t vDivisions = 20;
+  float uDel = 1.0f / uDivisions;
+  float vDel = 1.0f / vDivisions;
 
-  gluSphere(q, mRadius, 20, 20);
+  glScalef(mRadius, mRadius, mRadius);
+
+  glBegin(GL_QUADS);
+  float v = 0;
+  for(size_t j = 0; j != uDivisions; ++j, v += vDel)
+  {
+    float u = 0;
+    for(size_t i = 0; i != 20; ++i, u += uDel)
+    {
+      Point p;
+      float pdf;
+      Mappings<Point>::unitSquareToSphere(u, v,
+                                          Point(1,0,0),
+                                          Point(0,1,0),
+                                          Point(0,0,1),
+                                          p, pdf);
+
+      glNormal3fv(p);
+      glVertex3fv(p);
+
+      Mappings<Point>::unitSquareToSphere(u + uDel, v,
+                                          Point(1,0,0),
+                                          Point(0,1,0),
+                                          Point(0,0,1),
+                                          p, pdf);
+      glNormal3fv(p);
+      glVertex3fv(p);
+
+      Mappings<Point>::unitSquareToSphere(u + uDel, v + vDel,
+                                          Point(1,0,0),
+                                          Point(0,1,0),
+                                          Point(0,0,1),
+                                          p, pdf);
+      glNormal3fv(p);
+      glVertex3fv(p);
+
+      Mappings<Point>::unitSquareToSphere(u, v + vDel,
+                                          Point(1,0,0),
+                                          Point(0,1,0),
+                                          Point(0,0,1),
+                                          p, pdf);
+      glNormal3fv(p);
+      glVertex3fv(p);
+    } // end for
+  } // end for j
+  glEnd();
 
   glPopMatrix();
   glPopAttrib();
